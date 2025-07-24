e918a37c87de4d00f3824495139226e856b0fe5c
Pavlyysh <pgfu619@mail.ru>
2025-07-24 14:00:08 +0300

    mutex and waitGroup

---
+++ sync/sync.go
@@ -0,0 +1,24 @@
+package main
+
+import "sync"
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	c.value++
+	c.mu.Unlock()
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
+
+func main() {}
